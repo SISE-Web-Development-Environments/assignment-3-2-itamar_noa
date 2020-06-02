9ca7b540114cc51294b499a80691631f661bfe77
zigelitamar <zigelitamar@gmail.com>
2020-06-02 17:35:39 +0300

    start project

---
+++ main.js
@@ -0,0 +1,55 @@
+//  Import importent libraries
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const session = require("client-sessions");
+
+
+// Application settings
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// app.use(morgan(":method url :status :response-time ms"));
+// app.use(
+//     session({
+//       cookieName: "myCookie",
+//       secret: "magicWord24",
+//       duration:  10*1000,
+//       activeDuration: 0, 
+//     })
+// );
+
+//    Import resources
+// const auth = require("./routes/auth");
+const user = require("./routes/user");
+const recipes = require("./routes/recipes");
+
+
+// For checking server availabilty
+app.get("/alive",(req,res) => {
+    res.send("Im aliveeeeeeeeeeeeeeeeeeeeeeeeeee");
+});
+
+
+// Routing
+app.use("/user", user);
+app.use("/recipes", recipes);
+// app.use(auth); //without prefix! 
+
+//Default Router
+app.use((req,res) => {
+res.sendStatus(404);
+});
+
+// error middleware
+app.use(function (err, req, res, next) {
+    console.error(err);
+    res.status(500).send({ message: err.message, success: false });
+  });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
