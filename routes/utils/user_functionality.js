1dfc5c03b03a9767d537121dfe3c57ee2f1d5bdd
zigelitamar <zigelitamar@gmail.com>
2020-06-22 19:05:09 +0300

    apikey

--- routes/utils/user_functionality.js
+++ routes/utils/user_functionality.js
@@ -5,7 +5,7 @@ const DButils = require("./DButils");
 const recipesGetter = require("./search_recipes");
 
 const api_domain = "https://api.spoonacular.com/recipes";
-const api_key = "9020471b054d47a69c35d39e47668fa2";
+const api_key = "4b64b6b2b47242ce9b2583b0b1950cdb";
 
 async function getUserInfoOnRecipes(userid, ids) {
   let promises = {};
