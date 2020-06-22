1ad3f592fd8134ec9876b6832cc016cffa2d283f
zigelitamar <zigelitamar@gmail.com>
2020-06-22 13:10:50 +0300

    fix coockies - eran

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ app.use(
     duration: 20 * 60 * 1000, // expired after 20 minutes
     activeDuration: 0, // if expiresIn < activeDuration,
     //the session will be extended by activeDuration milliseconds
+    cookie: {
+      httpOnly: false,
+    },
   })
 );
 const corsConfig = {
